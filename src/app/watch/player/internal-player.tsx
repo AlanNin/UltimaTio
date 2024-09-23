cc71e3eeae98454c0fd005e7f7ffb3262bdb2442
AlanNin <shushubanaa@gmail.com>
2024-09-23 08:48:29 -0400

    fix crash

--- src/app/watch/player/internal-player.tsx
+++ src/app/watch/player/internal-player.tsx
@@ -2,12 +2,17 @@
 import React, { useRef } from "react";
 import "@vidstack/react/player/styles/default/theme.css";
 import "@vidstack/react/player/styles/default/layouts/video.css";
-import { MediaPlayer, MediaProvider, type MediaPlayerInstance, Track, isGoogleCastProvider, type MediaProviderAdapter, type MediaProviderSetupEvent, } from "@vidstack/react";
+import {
+  MediaPlayer,
+  MediaProvider,
+  type MediaPlayerInstance,
+  Track,
+} from "@vidstack/react";
 import {
   DefaultAudioLayout,
   defaultLayoutIcons,
   DefaultVideoLayout,
-} from '@vidstack/react/player/layouts/default';
+} from "@vidstack/react/player/layouts/default";
 
 type Props = {
   scrapData: any;
@@ -20,70 +25,75 @@ type Props = {
   watchProgress: any;
 };
 
-const InternalPlayer: React.FC<Props> = ({ scrapData, title, category, season, episode, handleCurrentTimeUpdate, handleDurationUpdate, watchProgress  }) => {
+const InternalPlayer: React.FC<Props> = ({
+  scrapData,
+  title,
+  category,
+  season,
+  episode,
+  handleCurrentTimeUpdate,
+  handleDurationUpdate,
+  watchProgress,
+}) => {
+  // define player ref
+  let playerRef = useRef<MediaPlayerInstance>(null);
 
-    // define player ref
-    let playerRef = useRef<MediaPlayerInstance>(null);
-
-    // define tracks
-    const tracks = scrapData?.stream.captions?.length
-    ? scrapData.stream.captions?.map((track: any) => ({
+  // define tracks
+  const tracks = scrapData?.stream?.captions?.length
+    ? scrapData?.stream?.captions?.map((track: any) => ({
         ...track,
         src: track.url, // Renombrar 'file' a 'src'
-        kind:"subtitles",
+        kind: "subtitles",
         label: track.language,
         file: undefined,
-        default: track.language.toLowerCase().includes("english")
+        default: track.language.toLowerCase().includes("english"),
       }))
     : [];
 
-    // define url
-    const url = scrapData?.stream?.playlist;
+  // define url
+  const url = scrapData?.stream?.playlist;
 
-    // define handlePlay
-    const handlePlay = () => {
-        const videoPlayer = playerRef.current;
-        handleDurationUpdate(videoPlayer?.duration)
+  // define handlePlay
+  const handlePlay = () => {
+    const videoPlayer = playerRef.current;
+    handleDurationUpdate(videoPlayer?.duration);
 
-        if (watchProgress && watchProgress > 0) {
-            videoPlayer!.currentTime = watchProgress;
+    if (watchProgress && watchProgress > 0) {
+      videoPlayer!.currentTime = watchProgress;
+    }
+    videoPlayer?.play();
+  };
+
+  return (
+    <div className="h-full w-full">
+      <MediaPlayer
+        title={
+          category === "movie" ? title : `${title} - S${season}E${episode}`
         }
-        videoPlayer?.play();
-    };
+        src={url}
+        autoPlay={true}
+        onCanPlay={handlePlay}
+        ref={playerRef}
+        onTimeUpdate={() =>
+          handleCurrentTimeUpdate(playerRef.current?.currentTime)
+        }
+        crossOrigin="anonymous"
+        googleCast={{
+          autoJoinPolicy: "origin-scope" as any,
+          language: "en-US",
+          receiverApplicationId: "...",
+        }}
+      >
+        <MediaProvider>
+          {tracks.length > 0 &&
+            tracks.map((track: any) => <Track {...track} key={track.src} />)}
+        </MediaProvider>
 
-    return (
-        <div className="w-full h-full">
-
-        <MediaPlayer
-            title={category === "movie" ? title : `${title} - S${season}E${episode}`}
-            src={url}
-            autoPlay={true}
-            onCanPlay={handlePlay}
-            ref={playerRef}
-            onTimeUpdate={() => handleCurrentTimeUpdate(playerRef.current?.currentTime)}
-            crossOrigin="anonymous"
-            googleCast={{
-              autoJoinPolicy: "origin-scope" as any,
-              language: 'en-US',
-              receiverApplicationId: '...',
-            }}
-        >
-            <MediaProvider>
-
-            {tracks.length > 0 &&
-            tracks.map((track: any) => (
-              <Track {...track} key={track.src} />
-            ))}
-            </MediaProvider>
-
-            <DefaultAudioLayout icons={defaultLayoutIcons} />
-            <DefaultVideoLayout
-            icons={defaultLayoutIcons}
-            />
-        </MediaPlayer>
-
-        </div>
-    );
+        <DefaultAudioLayout icons={defaultLayoutIcons} />
+        <DefaultVideoLayout icons={defaultLayoutIcons} />
+      </MediaPlayer>
+    </div>
+  );
 };
 
 export default InternalPlayer;
